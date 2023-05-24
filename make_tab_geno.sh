ff4b9b623d622d21b40702956473b7d960398010
Mari Johnson <66474570+mari-lynne@users.noreply.github.com>
2023-05-24 07:00:49 +0100

    Add files via upload
    Update tab format for bcf files

---
+++ make_tab_geno.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 3) Convert to tab-delim, phased
+
+# Columns: chr, position, hap1 allele, hap2 allele
+# VCF if genotype info is 0/1 then unphased, if stored as 0|1 = phased (i.e hap1 | hap2)
+# bcftools query -f '%CHROM\t%POS[\t%GT]\n' freeze10b.whi_only_chr21.bcf | head -3
+# bcftools query f'[%CHROM\t%POS\t%SAMPLE\t%GT\n]  freeze10b.whi_only_chr21.bcf | head -3
+
+
+# chr pos 0|1 >> need to find | in GT field, and replace delim with \t
+DIR=/fh/scratch/delete90/kooperberg_c/mjohnson/cseqtl/results/genotype/split
+OUTDIR=${DIR}/sub
+
+for file in `cat geno_files_filt.txt`;
+do cp $file $OUTDIR;
+done
+
+cd $OUTDIR
+
+for file in `cat geno_files_filt.txt`;
+do bcftools query -f '%CHROM\t%POS[\t%GT]\n' $file | sed 's:|:\t:g' -> ${file}_2;
+done
+
+# new header
