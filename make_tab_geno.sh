d4c1a3bab6aba2afa4be59fb8ff335829aea83a5
Mari Johnson <66474570+mari-lynne@users.noreply.github.com>
2023-05-25 18:34:53 -0700

    Update make_tab_geno.sh

--- make_tab_geno.sh
+++ make_tab_geno.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# 3) Convert to tab-delim, phased
-
+# 3) Convert phased bcf file to tab-delim phased file for aseq:
 # Columns: chr, position, hap1 allele, hap2 allele
-# VCF if genotype info is 0/1 then unphased, if stored as 0|1 = phased (i.e hap1 | hap2)
+# Use bcf tools query to output fields, and sed to find and replace delim
+
 # bcftools query -f '%CHROM\t%POS[\t%GT]\n' freeze10b.whi_only_chr21.bcf | head -3
-# bcftools query f'[%CHROM\t%POS\t%SAMPLE\t%GT\n]  freeze10b.whi_only_chr21.bcf | head -3
-
-
 # chr pos 0|1 >> need to find | in GT field, and replace delim with \t
+
 DIR=/fh/scratch/delete90/kooperberg_c/mjohnson/cseqtl/results/genotype/split
 OUTDIR=${DIR}/sub
 
@@ -21,9 +19,7 @@ echo "File pasting complete"
 cd $OUTDIR
 
 for file in `cat ${DIR}/geno_files_filt.txt`;
-do bcftools query -f '%CHROM\t%POS[\t%GT]\n' $file | sed 's:|:\t:g' -> ${file}_2;
+do bcftools query -f '%CHROM\t%POS[\t%GT]\n' $file | sed 's:|:\t:g' -> ${file};
 done
 
 echo "tab-delim haplotype files sorted"
-
-# new header
