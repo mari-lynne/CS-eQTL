5440f1378fa0587d5a1acc3cbb3102b1ee9a5e17
Mari Johnson <66474570+mari-lynne@users.noreply.github.com>
2023-09-14 17:29:31 -0700

    Update 2_eqtl_format_submit.sh

--- wgs-data-formatting/2_eqtl_format_submit.sh
+++ wgs-data-formatting/2_eqtl_format_submit.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=/fh/scratch/delete90/kooperberg_c/mjohnson/cseqtl/scripts
 cd ${IN_DIR}
 
 # Array of chromosomes
-chromosomes=("1" "2")
+chromosomes=("3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21")
 
 # Loop through all chromosomes and submit jobs
 for chr in "${chromosomes[@]}"; do
@@ -15,7 +15,7 @@ for chr in "${chromosomes[@]}"; do
   # Submit the sample jobs and capture the main job ID
   first_batch_job=$(sbatch --array=1-${num_samples}%50 --export=chr=${chr} ${SCRIPT_DIR}/2_eqtl-format-pt1.sh | awk '{print $NF}')
   
-  # Proceed aseq and cseqtl formatting scripts
+  # Proceed aseq and cseqtl formatting scripts (pt2/3 run in parallel)
   job2=$(sbatch \
   --parsable \
   --export=chr=$chr \
@@ -32,3 +32,4 @@ for chr in "${chromosomes[@]}"; do
   # Print out the job IDs for logging/debugging
   echo "For chromosome $chr, submitted jobs dependent on sample-level jobs and their IDs are $job2, $job3"
 done
+
