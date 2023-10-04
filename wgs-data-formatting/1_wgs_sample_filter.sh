58886cf0caf3990849389cc47c87b831d37959df
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2023-10-04 16:50:41 -0700

    edit comments, added biallelic snp filter earlier

--- wgs-data-formatting/1_wgs_sample_filter.sh
+++ wgs-data-formatting/1_wgs_sample_filter.sh
@@ -1,35 +1,32 @@
 #!/bin/bash
 
-# Steps ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# 1) Filter WHI particpants across chromsomes
+# Aims:
+# Filter participants from LLS study from the larger WHI cohort WGS data
 
 ml BCFtools/1.14-GCC-11.2.0
 
-# Set Directories and variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 1) Set Directories and variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# Input
-GENO_DIR=/fh/scratch/delete90/kooperberg_c/topmed_freeze10/phased/whi_only
-# Where the sample_file is saved
-IN_DIR=/fh/scratch/delete90/kooperberg_c/mjohnson/cseqtl/results/genotype
-SAMPLE_FILE=lls_geno_filt.txt
+GENO_DIR=/fh/scratch/delete90/kooperberg_c/topmed_freeze10/phased/whi_only # BCF File Input Directory
 GENO_FILE=freeze10b.whi_only_chr
 
-# OUTPUT
-OUT_DIR=${IN_DIR}/LLS
+IN_DIR=/fh/scratch/delete90/kooperberg_c/mjohnson/cseqtl/results/genotype # Sample_file/manifest Directory
+SAMPLE_FILE=lls_geno_filt.txt
 
-# Start Script
+OUT_DIR=${IN_DIR}/LLS/Sept
+
 # Check if directories exist
 if [ ! -d "$GENO_DIR" ] || [ ! -d "$IN_DIR" ] || [ ! -d "$OUT_DIR" ]; then
   echo "One or more directories do not exist. Exiting."
   exit 1
 fi
 
-# 1) Filter particpants across chromsomes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 2) Filter particpants across chromsomes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# For each chromsome bcf file subset participants an make a new bcf file
-# Each chromsome submits an new job (so we will have 23 jobs total)
-# Use @ notation to subset 1 dim array
+# Make a 1-dim array to store chromsome values (@ notation subsets array)
+# For each chromsome submit a new job (23 jobs total)
+# Then for each chromsome bcf file, subset participants and make a new bcf file
+# -m2 -M2 -v snps selects biallelic snps
 
 chr=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 X )
 for f in "${chr[@]}"
@@ -39,10 +36,10 @@ do
     --mail-type=BEGIN,END,FAIL \
     --output=Rout/%j.out \
     --error=Rerr/%j.err \
-    --wrap="bcftools view -Ob --force-samples \
+    --wrap="bcftools view -Ob -m2 -M2 -v snps --force-samples \
             -S ${IN_DIR}/${SAMPLE_FILE} \
             ${GENO_DIR}/${GENO_FILE}${f}.bcf > \
             ${OUT_DIR}/${GENO_FILE}${f}.bcf"
 done
 
-echo "Sample Filtering DONE"
+echo "Sample Filtering - DONE!"
