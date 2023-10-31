d305c51ef67d43c67aea41fc5caae327a57395d0
mari-lynne <mari.johnson@lmh.ox.ac.uk>
2023-10-30 17:21:27 -0700

    Updated run settings

--- 4_rnaseq_ase.sh
+++ 4_rnaseq_ase.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-#SBATCH --array=[1-21]
+#SBATCH --array=[1-1327]
+#SBATCH --job-name=aseq_data_prep
 #SBATCH --nodes=1
-#SBATCH --mem-per-cpu=32000
-#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20G
 #SBATCH --output=Rout/par-%j-%a.out
-#SBATCH --error=Rout/par-%j-%a.err
-#SBATCH --mail-type=begin
-#SBATCH --mail-type=end
-#SBATCH --mail-type=fail
+#SBATCH --error=Rerr/par-%j-%a.err
+#SBATCH --mail-type=begin,end,fail
 #SBATCH --mail-user=mjohnso5@fredhutch.org
 
 ml fhR/4.2.2.1-foss-2021b
 
-Rscript 4_rnaseq_ase.R $SLURM_JOB_NAME
+Rscript 2_rnaseq_ase.R $SLURM_JOB_NAME
 
-# Run in fh/scratch/delete90/kooperberg_c/mjohnson/cseqtl as sbatch -J manifest_rna_geno.csv 4_rnaseq_ase.sh
+# Subset = Run in fh/scratch/delete90/kooperberg_c/mjohnson/cseqtl as sbatch -J manifest_rna_geno.csv 4_rnaseq_ase.sh
+# LLS Run in fh/scratch/delete90/kooperberg_c/mjohnson/cseqtl/scripts/rnaseq_scripts as sbatch -J lls_rna_array.csv 2_rnaseq_ase.sh
 
